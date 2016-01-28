378daa2f7ac3697886e8ff2a646c6e0bdabf3001
Will Spurgeon <wespurgeon@wpi.edu>
2016-01-27 20:13:26 -0500

    Part 1 of the assignment is done. Easy right?

--- ann.py
+++ ann.py
@@ -1,1 +1,15 @@
-print "This line will be printed."
+import sys
+
+
+def main():
+    filename = sys.argv[1]
+    hiddenNodesNum = sys.argv[2]
+    holdoutPercentage = sys.argv[3]
+
+    print filename
+    print hiddenNodesNum
+    print holdoutPercentage
+
+
+if __name__ == "__main__":
+    main()
