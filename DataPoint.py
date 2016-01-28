2cd90064656a1c23debd55b45ef92b3e1b131cc8
Will Spurgeon <wespurgeon@wpi.edu>
2016-01-27 20:22:00 -0500

    Added the DataPoint class.

---
+++ DataPoint.py
@@ -0,0 +1,10 @@
+class DataPoint:
+    xValue = 0.0000
+    yValue = 0.0000
+    dataLabel = 0
+
+    def __init__(self, xValue, yValue, dataLabel):
+        self.xValue = xValue
+        self.yValue = yValue
+        self.dataLabel = dataLabel
+
